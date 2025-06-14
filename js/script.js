99b9d0ff1e2bc36d52f92d769e970057a2d8dbcb
Yuki Harada <yuki.freelife@gmail.com>
2025-06-14 20:56:55 +0900

    6/14作成

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const backToTop = document.getElementById('back-to-top');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        backToTop.style.display = 'block';
+    } else {
+        backToTop.style.display = 'none';
+    }
+});
+backToTop.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
